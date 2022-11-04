357f2522c841fd497b586f64f691fcd239615d93
kwrl <hakon@savvy.no>
2022-11-04 13:50:47 +0000

    auto-update - 2022-11-04 13:50:47

---
+++ _nuxt/static/1667569840/contact/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/contact", {data:[{_img:{"/_ipx/images/header-image-1.png?f=webp&q=100&s=628_790":"\u002F_nuxt\u002Fimage\u002Fa702cf.webp","/_ipx/images/graffiti_2.svg?f=webp&q=100&s=379_431":"\u002F_nuxt\u002Fimage\u002Fd1984c.webp"}}],fetch:{},mutations:void 0});
