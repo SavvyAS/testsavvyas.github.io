17debfdc4ad5bfc3b4fa80c1e1397e1930ae1983
kwrl <hakon@savvy.no>
2022-11-08 23:10:12 +0000

    auto-update: 2022-11-08 23:10:09

--- _next/static/chunks/app/contact/page-1988977f653f994d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{2202:function(n,e,u){Promise.resolve().then(u.t.bind(u,30,23))}},function(n){n.O(0,[30,17,744],function(){return n(n.s=2202)}),_N_E=n.O()}]);
--- _nuxt/static/1643973880/news/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/news", {data:[{articles:[],_img:{}}],fetch:{},mutations:void 0});
